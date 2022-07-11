6fa5e81df236ccaf5ce04dc3ad279961a789ba5a
Gusty1 <a0985209465@gmail.com>
2022-07-12 01:15:18 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,6 +9,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.uiColor = '#AADC6E';
 	config.extraPlugins = 'youtube';
 	config.extraPlugins = 'imgur';
-    config.imgurClientID = '0a4c1d7727faf54'; //You need to change this key with your's
+  config.imgurClientID = '0a4c1d7727faf54'; //You need to change this key with your's
 	config.removePlugins = 'easyimage,cloudservices,exportpdf';
+	config.font_names ='Arial/Arial, Helvetica, sans-serif;Comic Sans MS/Comic Sans MS, cursive;Courier New/Courier New, Courier, monospace;Georgia/Georgia, serif;Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;Tahoma/Tahoma, Geneva, sans-serif;Times New Roman/Times New Roman, Times, serif;Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;Verdana/Verdana, Geneva, sans-serif;新細明體;標楷體;微軟正黑體' ;
 };

d80cf4298cd3d62b152f52337e18794ea14d4b80
Gusty <a0985209465@gmail.com>
2024-08-04 23:42:29 +0800

    issue 建立

--- js/tool.js
+++ js/tool.js
@@ -127,28 +127,6 @@ function getIframeId () {
   return null // 如果没有找到，返回 null
 }
 
-function createIssue (title, url, label) {
-  console.log('createIssue')
-  fetch('https://api.github.com/repos/Gusty1/GustyLittleWorld/dispatches', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/vnd.github.v3+json',
-      'Authorization': atob('Z2hwX01pNGY1RXQ2YlF5dll2eVJRcnBsRUJOOERXZE03bDA3dUt6Sg=='),
-    },
-    body: JSON.stringify({
-      event_type: 'create-issue',
-      client_payload: {
-        title: title,
-        body: url,
-        labels: [label, 'Gitalk']
-      }
-    })
-  })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error))
-}
-
 // 下面是以後可能會用到的東西...
 
 // firebase auth 登出，以後可能有G會用到
